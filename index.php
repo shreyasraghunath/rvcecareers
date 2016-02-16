eccf6c62f485f36fb11087c0293e6b67e59408d8
Shreyas Raghunath <shreyasraghunath2007@gmail.com>
2016-02-16 20:45:01 +0530

    index changes

--- index.php
+++ index.php
@@ -1593,7 +1593,7 @@ div.selected {
 <option value="8">Uttar Pradesh</option>
 <option value="19">Uttarakhand</option>
 <option value="10">West Bengal</option>
-<option value="37">sdfasfd</option></select> 
+ 
 									     <input type="hidden" id="tempPermanentState" name="tempPermanentState" value='' />
 									     </td>
 									     <td align="left">
@@ -2535,7 +2535,48 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP0" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[0].yearOfComp" onchange="getSelectedYear(this.value,0,true)" id="YOP0" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[0].yearOfComp" onchange="getSelectedYear(this.value,0,true)" id="YOP0" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												</select>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[0].grade" maxlength="40" size="10" value="" class="textbox">
@@ -2563,7 +2604,50 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP1" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[1].yearOfComp" onchange="getSelectedYear(this.value,1,true)" id="YOP1" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[1].yearOfComp" onchange="getSelectedYear(this.value,1,true)" id="YOP1" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												</select>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[1].grade" maxlength="40" size="10" value="" class="textbox">
@@ -2591,7 +2675,52 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP2" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[2].yearOfComp" onchange="getSelectedYear(this.value,2,true)" id="YOP2" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[2].yearOfComp" onchange="getSelectedYear(this.value,2,true)" id="YOP2" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												<option value="2011">2011</option>
+												<option value="2012">2012</option>
+												</select>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[2].grade" maxlength="40" size="10" value="" class="textbox">
@@ -2619,7 +2748,54 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP3" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[3].yearOfComp" onchange="getSelectedYear(this.value,3,true)" id="YOP3" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[3].yearOfComp" onchange="getSelectedYear(this.value,3,true)" id="YOP3" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												<option value="2011">2011</option>
+												<option value="2012">2012</option>
+												<option value="2013">2013</option>
+												<option value="2014">2014</option>
+												</select>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[3].grade" maxlength="40" size="10" value="" class="textbox">
@@ -2647,7 +2823,57 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP4" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[4].yearOfComp" onchange="getSelectedYear(this.value,4,true)" id="YOP4" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[4].yearOfComp" onchange="getSelectedYear(this.value,4,true)" id="YOP4" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												<option value="2011">2011</option>
+												<option value="2012">2012</option>
+												<option value="2013">2013</option>
+												<option value="2014">2014</option>
+												<option value="2005">2015</option>
+												<option value="2016">2016</option>
+												
+												</select>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[4].grade" maxlength="40" size="10" value="" class="textbox">
@@ -2675,7 +2901,55 @@ div.selected {
 									
 									<td class="row-even" align="left">
 										<input type="hidden" id="HiddenYOP5" value="">
-											<select name="empResumeSubmissionTo.empQualificationFixedTo[5].yearOfComp" onchange="getSelectedYear(this.value,5,true)" id="YOP5" class="combolarge"><option value="" selected="selected">- Select -</option></select>
+											<select name="empResumeSubmissionTo.empQualificationFixedTo[5].yearOfComp" onchange="getSelectedYear(this.value,5,true)" id="YOP5" class="combolarge">
+												<option value="">Select Year</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												<option value="2011">2011</option>
+												<option value="2012">2012</option>
+												<option value="2013">2013</option>
+												<option value="2014">2014</option>
+												<option value="2005">2015</option>
+												<option value="2016">2016</option>
 									</td>
 									<td class="row-even" align="left">
 										<input type="text" name="empResumeSubmissionTo.empQualificationFixedTo[5].grade" maxlength="40" size="10" value="" class="textbox">
